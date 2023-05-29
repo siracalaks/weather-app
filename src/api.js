68e7b3f38f25adef41cfe9c1b9e4bb0de1039e78
Sirac Alakus <siracalaks@gmail.com>
2023-05-29 14:28:53 +0300

    weather app finish

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ef74c0d9fcmsh9aad6b08e54e1e2p15068fjsnbc2163e8b388',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "be6c138da64e43be65b0770135ce4db7";
